fe35a7f9bd9721f818512ccafa73ea7e0b9b642a
orewaee <orewaee@gmail.com>
2025-08-14 23:40:57 +0400

    fix: minor fixes

--- src/init/utils.rs
+++ src/init/utils.rs
@@ -6,13 +6,20 @@ const DEFAULT_STYLES: &'static str = include_str!("../../static/styles.css");
 const DEFAULT_TEMPLATE: &'static str = include_str!("../../templates/post.html");
 
 pub fn init() -> Result<(), InitError> {
-    fs::create_dir_all("static")
+    fs::create_dir("static")
         .map_err(|_| InitError::FailedToCreateDir)
         .expect("failed to create static dir");
-    fs::create_dir_all("templates")
+    fs::create_dir("templates")
         .map_err(|_| InitError::FailedToCreateDir)
         .expect("failed to create templates dir");
-    fs::write("static/styles.css", DEFAULT_STYLES).expect("failed to create default styles");
-    fs::write("templates/post.html", DEFAULT_TEMPLATE).expect("failed to create default template");
+    fs::create_dir("posts")
+        .map_err(|_| InitError::FailedToCreateDir)
+        .expect("failed to create posts dir");
+    fs::write("static/styles.css", DEFAULT_STYLES)
+        .map_err(|_| InitError::FailedToCreateFile)
+        .expect("failed to create default styles");
+    fs::write("templates/post.html", DEFAULT_TEMPLATE)
+        .map_err(|_| InitError::FailedToCreateFile)
+        .expect("failed to create default template");
     Ok(())
 }
