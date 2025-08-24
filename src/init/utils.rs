7602f27795b0f7a79168131a34b4fa21de8ef081
orewaee <orewaee@gmail.com>
2025-08-24 18:32:43 +0400

    fix: static styles.css is not applied

--- src/init/utils.rs
+++ src/init/utils.rs
@@ -2,7 +2,6 @@ use std::fs;
 
 use crate::init::{error::InitError, params::InitParams};
 
-const DEFAULT_STYLES: &'static str = include_str!("../../static/styles.css");
 const DEFAULT_TEMPLATE: &'static str = include_str!("../../templates/post.html");
 
 pub fn init(params: InitParams) -> Result<(), InitError> {
@@ -15,9 +14,6 @@ pub fn init(params: InitParams) -> Result<(), InitError> {
     fs::create_dir("posts")
         .map_err(|_| InitError::FailedToCreateDir)
         .expect("failed to create posts dir");
-    fs::write(params.static_path.join("styles.css"), DEFAULT_STYLES)
-        .map_err(|_| InitError::FailedToCreateFile)
-        .expect("failed to create default styles");
     fs::write("templates/post.html", DEFAULT_TEMPLATE)
         .map_err(|_| InitError::FailedToCreateFile)
         .expect("failed to create default template");
